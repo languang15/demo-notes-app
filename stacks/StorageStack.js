d0a779aa9ba17a4589754ce21c582a528af04ee7
Guang <languang@3c22fb8ba8a8.ant.amazon.com>
2021-10-19 19:38:17 +0100

    Enabling CORS

--- stacks/StorageStack.js
+++ stacks/StorageStack.js
@@ -3,7 +3,7 @@ import * as sst from "@serverless-stack/resources";
 export default class StorageStack extends sst.Stack { 
   // Public reference to the bucket
   bucket;
-  
+
   // Public reference to the table
   table;
   
@@ -20,6 +20,18 @@ export default class StorageStack extends sst.Stack {
     });
 
     // Create an S3 bucket
-    this.bucket = new sst.Bucket(this, "Uploads");
+    this.bucket = new sst.Bucket(this, "Uploads", { 
+      s3Bucket: {
+      // Allow client side access to the bucket from a different domain
+      cors: [ 
+        {
+          maxAge: 3000,
+          allowedOrigins: ["*"],
+          allowedHeaders: ["*"],
+          allowedMethods: ["GET", "PUT", "POST", "DELETE", "HEAD"],
+        }, 
+       ],
+      }, 
+    });
   } 
 }
